0d9ba8cc0f148d61b908f34ccd1e58b6588a3bfd
axom tools <adamowaissi055@gmail.com>
2026-07-14 02:48:41 +0300

    Create paypal.py

---
+++ modules/paypal.py
@@ -0,0 +1,11 @@
+import requests
+
+def check(email, timeout):
+    url = 'https://www.paypal.com/signin/'
+    data = {'login_email': email}
+    try:
+        r = requests.post(url, data=data, timeout=timeout, allow_redirects=False)
+        exists = 'email' in r.text.lower()
+    except:
+        exists = None
+    return {'service': 'paypal', 'exists': exists, 'url': 'https://paypal.com'}

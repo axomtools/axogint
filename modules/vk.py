01399f7e86ec0ec93229b22d12adc4fd78234b61
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:29:20 +0300

    Update vk.py

--- modules/vk.py
+++ modules/vk.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://vk.com/rest/method/auth.checkPhone'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('response', {}).get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'vk error: {str(e)}', file=sys.stderr)
     return {'service': 'vk', 'exists': exists, 'url': 'https://vk.com'}

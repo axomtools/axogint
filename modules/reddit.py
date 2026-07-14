9a77312c5a3c27e720623c6ead8fe0179a00a24b
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:25:52 +0300

    Update reddit.py

--- modules/reddit.py
+++ modules/reddit.py
@@ -1,12 +1,15 @@
 from utils import retryrequest
 import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.reddit.com/api/username_available.json?user=' + email.split('@')[0]
     try:
-        r = retryrequest('GET', url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         data = r.json()
         exists = not data
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'reddit error: {str(e)}', file=sys.stderr)
     return {'service': 'reddit', 'exists': exists, 'url': 'https://reddit.com'}

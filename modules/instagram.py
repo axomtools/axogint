f2ba4da156208a40092f4bfd2c6e5eaba90ea9eb
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:24:45 +0300

    Update instagram.py

--- modules/instagram.py
+++ modules/instagram.py
@@ -1,13 +1,17 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/'
     headers = {'x-requested-with': 'XMLHttpRequest'}
     data = {'email': email}
     try:
-        r = requests.post(url, headers=headers, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, headers=headers, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('email_is_taken', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'instagram error: {str(e)}', file=sys.stderr)
     return {'service': 'instagram', 'exists': exists, 'url': 'https://instagram.com'}

eb2fc5609690da6fbdd18f1840e7e3c701810e66
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:39:16 +0300

    Update patreon.py

--- modules/patreon.py
+++ modules/patreon.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.patreon.com/api/auth/check_email'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('email_taken', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'patreon error: {str(e)}', file=sys.stderr)
     return {'service': 'patreon', 'exists': exists, 'url': 'https://patreon.com'}

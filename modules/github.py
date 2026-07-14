96fd31ac70988b512a981abfee956a9a04b50611
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:25:25 +0300

    Update github.py

--- modules/github.py
+++ modules/github.py
@@ -1,10 +1,13 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://github.com/login?email=' + email
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         exists = 'This account has been' not in r.text
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'github error: {str(e)}', file=sys.stderr)
     return {'service': 'github', 'exists': exists, 'url': 'https://github.com'}

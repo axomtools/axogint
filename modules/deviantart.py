3f1e41428df8c1c5678164f69da5a2b14253db95
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:41:49 +0300

    Update deviantart.py

--- modules/deviantart.py
+++ modules/deviantart.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.deviantart.com/api/v1/oauth2/email/check'
     params = {'email': email}
     try:
-        r = requests.get(url, params=params, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, params=params, verbose=verbose)
         res = r.json()
         exists = res.get('status', '') == 'exists'
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'deviantart error: {str(e)}', file=sys.stderr)
     return {'service': 'deviantart', 'exists': exists, 'url': 'https://deviantart.com'}

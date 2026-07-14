84c68d29b879e5737a89347d2155b9e0450f31ec
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:26:51 +0300

    Update tumblr.py

--- modules/tumblr.py
+++ modules/tumblr.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.tumblr.com/svc/account/register'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('errors', {}).get('email', []) != []
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'tumblr error: {str(e)}', file=sys.stderr)
     return {'service': 'tumblr', 'exists': exists, 'url': 'https://tumblr.com'}

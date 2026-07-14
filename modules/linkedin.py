8655ca9970d1193c0734cde5a9b7ae4131cec3f7
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:25:08 +0300

    Update linkedin.py

--- modules/linkedin.py
+++ modules/linkedin.py
@@ -1,11 +1,14 @@
 from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.linkedin.com/checkpoint/lg/login-submit'
     data = {'email': email, 'session_key': email}
     try:
-        r = retryrequest('POST', url, timeout=timeout, data=data, allow_redirects=False)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, allow_redirects=False, verbose=verbose)
         exists = r.status_code == 302 and 'login' not in r.headers.get('location', '')
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'linkedin error: {str(e)}', file=sys.stderr)
     return {'service': 'linkedin', 'exists': exists, 'url': 'https://linkedin.com'}

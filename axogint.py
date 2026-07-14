8c9e8ce259aa018f2f425ae6f8a3ec24ad6469ca
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:19:51 +0300

    Update axogint.py

--- axogint.py
+++ axogint.py
@@ -1,7 +1,6 @@
 import argparse
 import json
 import sys
-from concurrent.futures import thread
 from core import runchecks
 from utils import formatoutput
 
@@ -12,10 +11,17 @@ def main():
     parser.add_argument('--timeout', type=int, default=10, help='request timeout')
     parser.add_argument('--output', choices=['json','table'], default='table', help='output format')
     parser.add_argument('--modules', nargs='+', help='limit to specific modules')
+    parser.add_argument('--proxy', help='proxy url e.g. socks5://127.0.0.1:9050')
+    parser.add_argument('--tor', action='store_true', help='use tor default proxy (127.0.0.1:9050)')
+    parser.add_argument('--dns', action='store_true', help='perform dns/mx check before modules')
+    parser.add_argument('--rate-limit', type=float, default=0, help='global delay between requests (seconds)')
+    parser.add_argument('--color', action='store_true', help='enable colored output')
+    parser.add_argument('--verbose', action='store_true', help='show detailed error messages')
     args = parser.parse_args()
 
-    results = runchecks(args.email, args.threads, args.timeout, args.modules)
-    formatoutput(results, args.output)
+    results = runchecks(args.email, args.threads, args.timeout, args.modules,
+                        args.proxy, args.tor, args.dns, args.rate_limit, args.verbose)
+    formatoutput(results, args.output, args.color)
 
 if __name__ == '__main__':
     main()

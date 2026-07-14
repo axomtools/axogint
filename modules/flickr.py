31f6f5f5276a6d942e9d1c5e69c68db315528f9c
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:27:27 +0300

    Update flickr.py

--- modules/flickr.py
+++ modules/flickr.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://identity.flickr.com/checkusername'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('code') == 3
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'flickr error: {str(e)}', file=sys.stderr)
     return {'service': 'flickr', 'exists': exists, 'url': 'https://flickr.com'}

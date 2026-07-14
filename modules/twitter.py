62b9d200893f225afc97b0c72b4e93aaa428d779
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:24:13 +0300

    Update twitter.py

--- modules/twitter.py
+++ modules/twitter.py
@@ -1,11 +1,15 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://api.twitter.com/i/users/email_available.json?email=' + email
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         data = r.json()
         exists = not data.get('valid', True)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'twitter error: {str(e)}', file=sys.stderr)
     return {'service': 'twitter', 'exists': exists, 'url': 'https://twitter.com'}

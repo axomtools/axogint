13dab8291217520086ad27ac3df2d0f97b14f5d7
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:15:58 +0300

    Update reddit.py

--- modules/reddit.py
+++ modules/reddit.py
@@ -1,9 +1,10 @@
-import requests
+from utils import retryrequest
+import json
 
 def check(email, timeout):
     url = 'https://www.reddit.com/api/username_available.json?user=' + email.split('@')[0]
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, timeout=timeout)
         data = r.json()
         exists = not data
     except:

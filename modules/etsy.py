0d771b05631513069b8c730d7e2b27f1de95b335
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:39:34 +0300

    Update etsy.py

--- modules/etsy.py
+++ modules/etsy.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.etsy.com/api/v3/ajax/email/check'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('email_exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'etsy error: {str(e)}', file=sys.stderr)
     return {'service': 'etsy', 'exists': exists, 'url': 'https://etsy.com'}

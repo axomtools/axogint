459170ee283eb06a9a9a4960a8402a604e9c520c
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:37:21 +0300

    Update gravatar.py

--- modules/gravatar.py
+++ modules/gravatar.py
@@ -1,12 +1,15 @@
-import requests
+from utils import retryrequest
 import hashlib
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     h = hashlib.md5(email.strip().lower().encode('utf-8')).hexdigest()
     url = 'https://www.gravatar.com/avatar/' + h + '?d=404&s=1'
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         exists = r.status_code == 200
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'gravatar error: {str(e)}', file=sys.stderr)
     return {'service': 'gravatar', 'exists': exists, 'url': 'https://gravatar.com/' + h}

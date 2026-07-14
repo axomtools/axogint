db64488d9410f34b2afb9a0e1fa2179de6bc90f9
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:23:10 +0300

    Update google.py

--- modules/google.py
+++ modules/google.py
@@ -1,10 +1,12 @@
-import requests
+from utils import retryrequest
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://mail.google.com/mail/gxlu?email=' + email
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         exists = 'Success' in r.text
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'google error: {str(e)}', file=sys.stderr)
     return {'service': 'google', 'exists': exists, 'url': 'https://mail.google.com'}

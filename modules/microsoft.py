6daab836534f95f082a01e7a0a3ab70b750b429c
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:29:42 +0300

    Update microsoft.py

--- modules/microsoft.py
+++ modules/microsoft.py
@@ -1,10 +1,13 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&email=' + email
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         exists = 'invalid email' not in r.text.lower()
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'microsoft error: {str(e)}', file=sys.stderr)
     return {'service': 'microsoft', 'exists': exists, 'url': 'https://outlook.com'}

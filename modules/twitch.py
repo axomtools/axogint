69b9344b27a0ed739f3b4cd294990722ffbdb404
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:38:56 +0300

    Update twitch.py

--- modules/twitch.py
+++ modules/twitch.py
@@ -1,10 +1,13 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://id.twitch.tv/oauth2/validate?email=' + email
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         exists = r.status_code != 400
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'twitch error: {str(e)}', file=sys.stderr)
     return {'service': 'twitch', 'exists': exists, 'url': 'https://twitch.tv'}

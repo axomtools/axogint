d90e6f2c7d6e672e08ae995d0063deb6228c6d97
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:38:22 +0300

    Update paypal.py

--- modules/paypal.py
+++ modules/paypal.py
@@ -1,11 +1,14 @@
 from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.paypal.com/signin/'
     data = {'login_email': email}
     try:
-        r = retryrequest('POST', url, timeout=timeout, data=data, allow_redirects=False)
-        exists = 'email' in r.text.lower()
-    except:
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, allow_redirects=False, verbose=verbose)
+        exists = r.status_code == 302
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'paypal error: {str(e)}', file=sys.stderr)
     return {'service': 'paypal', 'exists': exists, 'url': 'https://paypal.com'}

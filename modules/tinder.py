71578affbcf4a268724e2af79a9b71bda03a1519
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:43:58 +0300

    Update tinder.py

--- modules/tinder.py
+++ modules/tinder.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://api.gotinder.com/v2/auth/email'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('data', {}).get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'tinder error: {str(e)}', file=sys.stderr)
     return {'service': 'tinder', 'exists': exists, 'url': 'https://tinder.com'}

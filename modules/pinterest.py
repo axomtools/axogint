804c64b2e5aa5fc9b1dd5a935a4976a3ac14f4ed
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:26:31 +0300

    Update pinterest.py

--- modules/pinterest.py
+++ modules/pinterest.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.pinterest.com/resource/EmailExistsResource/get/'
     params = {'email': email}
     try:
-        r = requests.get(url, params=params, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, params=params, verbose=verbose)
         data = r.json()
         exists = data.get('resource_response', {}).get('data', {}).get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'pinterest error: {str(e)}', file=sys.stderr)
     return {'service': 'pinterest', 'exists': exists, 'url': 'https://pinterest.com'}

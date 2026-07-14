c1853570cd28398e4a92dc515188d901f5297022
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:40:42 +0300

    Update stackoverflow.py

--- modules/stackoverflow.py
+++ modules/stackoverflow.py
@@ -1,11 +1,14 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://stackoverflow.com/users/account-recovery'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout, allow_redirects=False)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, allow_redirects=False, verbose=verbose)
         exists = r.status_code == 302
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'stackoverflow error: {str(e)}', file=sys.stderr)
     return {'service': 'stackoverflow', 'exists': exists, 'url': 'https://stackoverflow.com'}

77f4e52bd2c317dce4f892ca09f164e98547e6f1
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:37:43 +0300

    Update ebay.py

--- modules/ebay.py
+++ modules/ebay.py
@@ -1,11 +1,14 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.ebay.com/signin/'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout, allow_redirects=False)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, allow_redirects=False, verbose=verbose)
         exists = r.status_code == 302
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'ebay error: {str(e)}', file=sys.stderr)
     return {'service': 'ebay', 'exists': exists, 'url': 'https://ebay.com'}

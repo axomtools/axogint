0499be5db1233e1f0550a0e121a1fd8f8c7ce69d
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:30:37 +0300

    Update wordpress.py

--- modules/wordpress.py
+++ modules/wordpress.py
@@ -1,11 +1,15 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://public-api.wordpress.com/rest/v1.1/users/email/' + email + '/exists'
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         res = r.json()
         exists = res.get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'wordpress error: {str(e)}', file=sys.stderr)
     return {'service': 'wordpress', 'exists': exists, 'url': 'https://wordpress.com'}

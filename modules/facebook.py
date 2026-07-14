89cc71c4f5323f23c5258d8bad502c72c1c75923
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:23:43 +0300

    Update facebook.py

--- modules/facebook.py
+++ modules/facebook.py
@@ -1,10 +1,13 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.facebook.com/login/identify/?ctx=recover&email=' + email
     try:
-        r = requests.get(url, timeout=timeout)
+        r = retryrequest('GET', url, session, timeout=timeout, verbose=verbose)
         exists = 'account not found' not in r.text.lower()
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'facebook error: {str(e)}', file=sys.stderr)
     return {'service': 'facebook', 'exists': exists, 'url': 'https://facebook.com'}

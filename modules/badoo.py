4b7bec14958dc411a89dd0baf5d99d8e969e0bad
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:43:36 +0300

    Update badoo.py

--- modules/badoo.py
+++ modules/badoo.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://badoo.com/api/v1/email/exists'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'badoo error: {str(e)}', file=sys.stderr)
     return {'service': 'badoo', 'exists': exists, 'url': 'https://badoo.com'}

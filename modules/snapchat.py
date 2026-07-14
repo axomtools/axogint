67f768896b0305b32fbb1dfec04f05e4c17c9d26
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:28:09 +0300

    Update snapchat.py

--- modules/snapchat.py
+++ modules/snapchat.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://accounts.snapchat.com/accounts/email_exists'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'snapchat error: {str(e)}', file=sys.stderr)
     return {'service': 'snapchat', 'exists': exists, 'url': 'https://snapchat.com'}

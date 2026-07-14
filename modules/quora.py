c2bb6f3383cc1273704d19cf1835847ddaa24f18
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:40:09 +0300

    Update quora.py

--- modules/quora.py
+++ modules/quora.py
@@ -1,12 +1,16 @@
-import requests
+from utils import retryrequest
+import json
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.quora.com/api/v3/email/exists'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, verbose=verbose)
         res = r.json()
         exists = res.get('exists', False)
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'quora error: {str(e)}', file=sys.stderr)
     return {'service': 'quora', 'exists': exists, 'url': 'https://quora.com'}

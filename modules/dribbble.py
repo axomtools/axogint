c6e41c332df2d5dd5724220031346351ecd90c12
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:41:09 +0300

    Update dribbble.py

--- modules/dribbble.py
+++ modules/dribbble.py
@@ -1,11 +1,14 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://dribbble.com/session'
     data = {'user[email]': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout, allow_redirects=False)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, allow_redirects=False, verbose=verbose)
         exists = r.status_code == 302
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'dribbble error: {str(e)}', file=sys.stderr)
     return {'service': 'dribbble', 'exists': exists, 'url': 'https://dribbble.com'}

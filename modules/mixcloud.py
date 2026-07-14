c207439bd72f229a5782454e3fb36e8e6afddbf7
axom tools <adamowaissi055@gmail.com>
2026-07-14 03:43:17 +0300

    Update mixcloud.py

--- modules/mixcloud.py
+++ modules/mixcloud.py
@@ -1,11 +1,14 @@
-import requests
+from utils import retryrequest
+import sys
 
-def check(email, timeout):
+def check(email, timeout, session, verbose):
     url = 'https://www.mixcloud.com/register/'
     data = {'email': email}
     try:
-        r = requests.post(url, data=data, timeout=timeout, allow_redirects=False)
+        r = retryrequest('POST', url, session, timeout=timeout, data=data, allow_redirects=False, verbose=verbose)
         exists = r.status_code == 302
-    except:
+    except Exception as e:
         exists = None
+        if verbose:
+            print(f'mixcloud error: {str(e)}', file=sys.stderr)
     return {'service': 'mixcloud', 'exists': exists, 'url': 'https://mixcloud.com'}
